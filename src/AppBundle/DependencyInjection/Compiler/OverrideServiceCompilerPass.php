481b8c05a1adc66a11a4a00e3e0eebe84db9926b
Utku Aydın <utkuaydin34@gmail.com>
2015-09-21 15:01:22 +0300

    configure appbundle DI settings

---
+++ src/AppBundle/DependencyInjection/Compiler/OverrideServiceCompilerPass.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class OverrideServiceCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+    }
+}
